44a211ad332fdbbdb99586cf5b06b594427e6dc6
suraj_dell_linux <surazmah@gmail.com>
2023-01-21 15:49:35 -0800

    remove pg commands

--- pg_commands.sh
+++
@@ -1,5 +0,0 @@
-docker volume create pg_vol
-docker run -d --name=pg14 -p 5432:5432 -v pg_vol:/var/lib/postgresql/data -e POSTGRES_PASSWORD=pass postgres:14.6
-docker exec -it pg14 psql -U postgres
-
-psql -h localhost -U postgres
