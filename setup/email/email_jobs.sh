d69c70acff04d8aa8f43356c44159921ae99d3d1
suraj_dell_linux <surazmah@gmail.com>
2023-02-01 21:34:39 -0800

    delete old scripts and add do cd from email cron j

--- setup/email/email_jobs.sh
+++ setup/email/email_jobs.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-#make it available for both test and prod
+#make it available for both test and prod in the future
+
+#Figure out how to cd to relative path; need to delete this absolute strategy
+cd /home/suraj/personal_projects/jobs_tracker/
+source setenv.sh test
 
 docker compose up database_test test_entrypoint -d
 docker exec test_box bash -c "python email_utils/send_email_utils.py"
