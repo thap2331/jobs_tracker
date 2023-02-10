b18d06a010505fe1abd6c7b1095d2bd32163bc3f
suraj_dell_linux <surazmah@gmail.com>
2023-02-09 16:24:52 -0800

    make things look pretty

--- utils/utils.py
+++ utils/utils.py
@@ -7,7 +7,8 @@ import pytz
 class DateTimeUtils:
 
     def convert_utc_to_pst(self, utc_time):
-        utc_time = pytz.utc.localize(utc_time)
-        pst_time = utc_time.astimezone(pytz.timezone("US/Pacific"))
+        utc_time                    = pytz.utc.localize(utc_time)
+        pst_time_datetime_instance  = utc_time.astimezone(pytz.timezone("US/Pacific"))
+        pst_time                    = pst_time_datetime_instance.strftime("%Y-%m-%d %H:%M:%S")
 
         return pst_time
